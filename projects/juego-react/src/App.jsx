67eeed981f49b7d21235e043e4efd969e93f65bd
David Cortés <daviidcl11@gmail.com>
2024-09-11 02:23:33 +0200

    Tic Tac Toe casi terminado

--- projects/juego-react/src/App.jsx
+++ projects/juego-react/src/App.jsx
@@ -1,27 +1,65 @@
-import './App.css'
+import { useState } from 'react'
+import conffeti from "canvas-confetti"
+import {Square} from "./components/Square.jsx"
+import {TURNS} from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const board = Array(9).fill(null)
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+
+  const updateBoard = (index) => {
+
+    if(board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    const newWinner = checkWinnerFrom(newBoard)
+    if(newWinner){
+      conffeti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)){
+      setWinner(false)
+    }
+  }
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className="game">
         {
-          board.map((_, index)=>{
-            <div className="cell" key={index}>
-              <span className="cell_content">
-                {index}
-              </span>
-            </div>
+          board.map((square, index) => {
+            return(
+              <Square 
+                key={index} 
+                index={index}
+                updateBoard={updateBoard}
+                > {square} </Square>
+            )
           })
         }
       </section>
+
+      <section className="turn">
+        <Square isSelected={turn == TURNS.X}> {TURNS.X} </Square>
+        <Square isSelected={turn == TURNS.O}> {TURNS.O} </Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame}/>
     </main>
   )
 }
