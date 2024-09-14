eed378cd573ad4af940ea1838e8aaf0df9b08317
David Cortés <daviidcl11@gmail.com>
2024-09-14 13:40:21 +0200

    vite update

--- projects/portfolio-react/vite.config.js
+++ projects/portfolio-react/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://davidcortesdev.github.io/react-projects"
+  base: "https://github.com/davidcortesdev/react-projects"
 })
