fa15cbbbf9f2f4b868a4b1418a84e550a7ab4e26
davidcortesdev <davidcortes.dev@gmail.com>
2024-10-26 12:47:25 +0200

    useContext

--- projects/hooks-app/src/main.jsx
+++ projects/hooks-app/src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 //import { Padre } from './07-tarea-memo/Padre'
 //import { Memorize } from './06-memos/Memorize'
@@ -13,10 +14,11 @@ import './index.css'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { CallbackHook } from './06-memos/CallbackHook'
 //import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
+//import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <TodoApp />
-  </>
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
 )
